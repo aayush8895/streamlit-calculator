444752088a4081b1dfc55d2e830880320dcca4b7
aayush8895 <aayush8895@gmail.com>
2025-12-29 15:09:15 +0530

    new changes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def calculator (n1, n2, operation):
         return n1 / n2
 
 
-st.title("Simple calculator V3")
+st.title("Simple calculator V4")
 
 number1 = st.number_input("Enter First number", value = 0.0)
 number2 = st.number_input("Enter Second number", value = 0.0)
